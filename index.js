523c2e6fe87e8fb32fd18accbb90f28eef014459
Nagendra Kumar <nagendra.msft@hotmail.com>
2017-10-18 12:51:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -395,9 +395,9 @@ function sendrate(sender, pageid) {
             			"title":"Welcome to Peter\'s Hats",
             			"image_url":"https://petersfancybrownhats.com/company_image.png",
             			"subtitle":"We have got the right hat for everyone.",
-            			"buttons":[{"title":"Small SB", "type":"web_url", "webview_height_ratio": "tall",  "url":"https://www.hdragomir.sb.messenger.com/marketplace_rating/popover"},
-					  {"title":"Small Intern", "type":"web_url", "webview_height_ratio": "compact",  "url":"https://www.intern.messenger.com/marketplace_rating/popover"},
-					   {"title":"Small PROD", "type":"web_url", "webview_height_ratio": "compact",  "url":"https://www.messenger.com/marketplace_rating/popover"}
+            			"buttons":[{"title":"Small SB", "type":"web_url", "webview_height_ratio": "compact",  "url":"https://www.hdragomir.sb.messenger.com/marketplace_rating/popover/"},
+					  {"title":"Small Intern", "type":"web_url", "webview_height_ratio": "compact",  "url":"https://www.intern.messenger.com/marketplace_rating/popover/"},
+					   {"title":"Small PROD", "type":"web_url", "webview_height_ratio": "compact",  "url":"https://www.messenger.com/marketplace_rating/popover/"}
 					  ]
           		        }				
 		       ]
