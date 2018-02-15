4c33ffe79aa2e00980415d37cb5e55cadbe15f65
Nagendra Kumar <nagendra.msft@hotmail.com>
2018-02-14 16:50:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.post('/webhook/', function (req, res) {
 	let messaging_events = req.body.entry[0].messaging
 	for (let i = 0; i < messaging_events.length; i++) {
 		let event = req.body.entry[0].messaging[i]
+		       console.log("Dumpping messaging field");
 			console.log(event.message);
 
 		let sender = event.sender.id
